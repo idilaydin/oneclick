3450e0c1b6cf6de955581e7c90240d061d505224
idilaydin <idilaydin@gmail.com>
2022-01-26 23:53:42 -0500

    Updates

---
+++ component---src-pages-404-js-d2903b0d9380ff0c3475.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkoneclick=self.webpackChunkoneclick||[]).push([[883],{9616:function(e,t,n){n.r(t);var l=n(7294),u=n(5691),c=n(3751);t.default=function(){return l.createElement(u.Z,null,l.createElement(c.Z,{title:"404: Not found"}),l.createElement("h1",null,"404: Not Found"),l.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-d2903b0d9380ff0c3475.js.map
---
+++ component---src-pages-page-2-js-e9c984999a7c9d8a51f5.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkoneclick=self.webpackChunkoneclick||[]).push([[617],{9863:function(e,t,n){n.r(t);var l=n(7294),c=n(5444),a=n(5691),o=n(3751);t.default=function(){return l.createElement(a.Z,null,l.createElement(o.Z,{title:"Page two"}),l.createElement("h1",null,"Hi from the second page"),l.createElement("p",null,"Welcome to page 2"),l.createElement(c.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-e9c984999a7c9d8a51f5.js.map
