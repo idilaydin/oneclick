b8615a0cde7a1fda18d4e569b0ddfe884f81d5a6
idilaydin <idilaydin@gmail.com>
2022-01-26 22:41:49 -0500

    Updates

---
+++ component---src-pages-404-js-9676f139c13fc003acba.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkoneclick=self.webpackChunkoneclick||[]).push([[883],{9616:function(e,t,n){n.r(t);var l=n(7294),u=n(481),c=n(3751);t.default=function(){return l.createElement(u.Z,null,l.createElement(c.Z,{title:"404: Not found"}),l.createElement("h1",null,"404: Not Found"),l.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-9676f139c13fc003acba.js.map
